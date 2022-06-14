20cd46265258bbf738115d892c32535f356f5d0c
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2022-06-14 13:09:14 +0200

    :bug: Fix F1 computation, move save and load to separate functions

--- dictionary_creator.py
+++ dictionary_creator.py
@@ -19,11 +19,28 @@ class DictionaryCreator(object):
         self.source_qids_by_word = None
         self.source_question_by_qid = None
 
-        self.target_words_by_qid = None
-        self.target_words_by_qid = None
+        self.gt_target_words_by_qid = None
+        self.aligned_target_words_by_qid = None
         self.target_qids_by_word = None
 
         self.top_tfidfs_by_qid = None
+
+    def _save_state(self):
+        with open(os.path.join(self.data_path, 'dc_state.pkl'), 'wb') as pklfile:
+            pickle.dump((self.gt_target_words_by_qid,
+                         self.source_qids_by_word,
+                         self.aligned_target_words_by_qid,
+                         self.target_qids_by_word,
+                         self.top_tfidfs_by_qid),
+                        pklfile)
+
+    def _load_state(self):
+        with open(os.path.join(self.data_path, 'dc_state.pkl'), 'rb') as pklfile:
+            (self.source_qids_by_word,
+             self.gt_target_words_by_qid,
+             self.aligned_target_words_by_qid,
+             self.target_qids_by_word,
+             self.top_tfidfs_by_qid) = pickle.load(pklfile)
 
     def dc_preprocessing(self, save=False):
         source_bible = 'eng-eng-kjv.txt'
@@ -71,7 +88,7 @@ class DictionaryCreator(object):
             return qid_by_word, words_by_qid, question_by_qid
 
         self.source_qids_by_word, _, self.source_question_by_qid = build_semdoms(df_source)
-        self.target_qids_by_word, self.target_words_by_qid, _ = build_semdoms(df_target)
+        self.target_qids_by_word, self.gt_target_words_by_qid, _ = build_semdoms(df_target)
 
         def tokenize_all_verses():
             # optional: Use tokenizer from huggingface
@@ -138,30 +155,25 @@ class DictionaryCreator(object):
                         matched_qids = {*matched_qids, *new_qids}
             return target_words_by_qid
 
-        self.target_words_by_qid = map_target_words_to_semdoms()
+        self.aligned_target_words_by_qid = map_target_words_to_semdoms()
 
         def show_mapped_words():
             idx = 1  # idx <= 4505 # 7938 questions, 1915 semdoms (for eng-deu)
-            semdom_name = list(self.target_words_by_qid.keys())[idx]
+            semdom_name = list(self.aligned_target_words_by_qid.keys())[idx]
             print(semdom_name)
-            print(list((self.target_words_by_qid.values()))[idx])
+            print(list((self.aligned_target_words_by_qid.values()))[idx])
 
         show_mapped_words()
 
         if save:
-            with open(os.path.join(self.data_path, 'dc_preprocessing_output.pkl'), 'wb') as pklfile:
-                pickle.dump((self.target_words_by_qid, self.source_qids_by_word,
-                             self.target_qids_by_word, self.target_words_by_qid),
-                            pklfile)
+            self._save_state()
 
     def dc_train_tfidf_based_model(self, load=False, save=False):
         if load:
-            with open(os.path.join(self.data_path, 'dc_preprocessing_output.pkl'), 'rb') as pklfile:
-                (self.target_words_by_qid, self.source_qids_by_word,
-                 self.target_qids_by_word, self.target_words_by_qid) = pickle.load(pklfile)
+            self._load_state()
 
         def compute_tfidfs():
-            tfidfs = self.vectorizer.fit_transform(list(self.target_words_by_qid.values()))
+            tfidfs = self.vectorizer.fit_transform(list(self.aligned_target_words_by_qid.values()))
             print(len(self.vectorizer.stop_words_))
             print(self.vectorizer.get_feature_names_out())
             print(tfidfs.shape)
@@ -172,7 +184,7 @@ class DictionaryCreator(object):
         def build_top_tfidfs():
             top_tfidfs_by_qid = {}
             for idx, tfidf in tqdm(enumerate(tfidfs), desc='collecting top tf-idf scores', total=tfidfs.shape[0]):
-                qid = list(self.target_words_by_qid.keys())[idx]
+                qid = list(self.aligned_target_words_by_qid.keys())[idx]
                 df = pd.DataFrame(tfidf.T.todense(), index=self.vectorizer.get_feature_names_out(), columns=["TF-IDF"])
                 df = df.sort_values('TF-IDF', ascending=False)
                 # TODO: cut off words with score 0.0
@@ -184,8 +196,7 @@ class DictionaryCreator(object):
         sorted(self.top_tfidfs_by_qid.items())[0]
 
         if save:
-            with open(os.path.join(self.data_path, 'dc_train_tfidf_based_model_output.pkl'), 'wb') as pklfile:
-                pickle.dump(self.top_tfidfs_by_qid, pklfile)
+            self._save_state()
 
     def dc_evaluate(self, load=False):
         """ H2 EVALUATE
@@ -214,11 +225,7 @@ class DictionaryCreator(object):
         """ load source and corresponding target words from Purdue Team (ground truth data for dictionary creation) """
 
         if load:
-            with open(os.path.join(self.data_path, 'dc_preprocessing_output.pkl'), 'rb') as pklfile:
-                (self.target_words_by_qid, self.source_qids_by_word,
-                 self.target_qids_by_word, self.target_words_by_qid) = pickle.load(pklfile)
-            with open(os.path.join(self.data_path, 'dc_train_tfidf_based_model_output.pkl'), 'rb') as pklfile:
-                self.top_tfidfs_by_qid = pickle.load(pklfile)
+            self._load_state()
 
         def load_test_data():
             df_test = pd.read_csv(f'{self.data_path}/multilingual_semdom_dictionary.csv')
@@ -278,7 +285,7 @@ class DictionaryCreator(object):
         """ remove all target words with a TF-IDF value below a threshold """
 
         def filter_target_semdoms_with_threshold():
-            threshold = 0.5
+            threshold = 0.02
             filtered_target_semdoms = dict()
             for qid, tf_idfs_df in self.top_tfidfs_by_qid.items():
                 filtered_target_semdoms[qid] = list(tf_idfs_df[tf_idfs_df['TF-IDF'] > threshold].index.values)
@@ -298,7 +305,7 @@ class DictionaryCreator(object):
                                    total=len(predicted_target_words_by_qid)):
                 num_positive_words += len(words)
                 for word in words:
-                    num_true_positive_words += word in self.target_words_by_qid[qid]
+                    num_true_positive_words += word in self.gt_target_words_by_qid.get(qid, [])
 
             num_ground_truth_target_words = 0
             for _, semdoms in tqdm(self.target_qids_by_word.items(),
@@ -322,166 +329,9 @@ class DictionaryCreator(object):
         compute_f1_score()
 
 
-def sid_with_word_clustering():
-    # Author: Olivier Grisel <olivier.grisel@ensta.org>
-    #         Lars Buitinck
-    #         Chyi-Kwei Yau <chyikwei.yau@gmail.com>
-    # License: BSD 3 clause
-
-    from time import time
-    import matplotlib.pyplot as plt
-    import math
-
-    from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-    from sklearn.decomposition import NMF, LatentDirichletAllocation
-    from sklearn.datasets import fetch_20newsgroups
-
-    n_samples = 2000
-    n_features = 10000
-    n_components = 10
-    n_top_words = 20
-
-    def plot_top_words(model, feature_names, n_top_words, title):
-        rows = math.ceil(n_components / 5)
-        fig, axes = plt.subplots(rows, 5, figsize=(30, 15), sharex=True)
-        axes = axes.flatten()
-        for topic_idx, topic in enumerate(model.components_):
-            top_features_ind = topic.argsort()[: -n_top_words - 1: -1]
-            top_features = [feature_names[i] for i in top_features_ind]
-            weights = topic[top_features_ind]
-            # print(top_features)
-            # continue
-
-            ax = axes[topic_idx]
-            ax.barh(top_features, weights, height=0.7)
-            ax.set_title(f"Topic {topic_idx + 1}", fontdict={"fontsize": 30})
-            ax.invert_yaxis()
-            ax.tick_params(axis="both", which="major", labelsize=20)
-            for i in "top right left".split():
-                ax.spines[i].set_visible(False)
-            fig.suptitle(title, fontsize=40)
-
-        # return
-        plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
-        plt.show()
-
-    # Load the 20 newsgroups dataset and vectorize it. We use a few heuristics
-    # to filter out useless terms early on: the posts are stripped of headers,
-    # footers and quoted replies, and common English words, words occurring in
-    # only one document or in at least 95% of the documents are removed.
-
-    # print("Loading dataset...")
-    # t0 = time()
-    # data, _ = fetch_20newsgroups(
-    #    shuffle=True,
-    #    random_state=1,
-    #    remove=("headers", "footers", "quotes"),
-    #    return_X_y=True,
-    # )
-    # data_samples = data[:n_samples]
-    # print("done in %0.3fs." % (time() - t0))
-
-    data_samples = []
-
-    ## verse concatenation approach
-    # step_size = 1
-    # for idx in range(0, len(target_verses), step_size):
-    #    data_samples.append(" ".join(target_verses[idx:idx+step_size]))
-
-    ## sliding window approch
-    # n_gram_size = 7
-    # slide = 3
-    # for verse in source_verses:
-    # tokens = tokenizer_function(verse)
-    # for idx in range(0, max(1, len(tokens) - n_gram_size + 1), slide):
-    # data_samples.append(' '.join(tokens[idx:idx+n_gram_size]))
-
-    data_samples = source_verses  # target_verses
-    data_samples
-
-    # Use tf-idf features for NMF.
-    print("Extracting tf-idf features for NMF...")
-    tfidf_vectorizer = TfidfVectorizer(
-        max_df=0.005, min_df=2,  # max_features=n_features, stop_words="english"
-    )
-    t0 = time()
-    tfidf = tfidf_vectorizer.fit_transform(data_samples)
-    print("done in %0.3fs." % (time() - t0))
-
-    # Use tf (raw term count) features for LDA.
-    print("Extracting tf features for LDA...")
-    tf_vectorizer = CountVectorizer(
-        max_df=0.005, min_df=2,  # max_features=n_features, stop_words="english"
-    )
-    t0 = time()
-    tf = tf_vectorizer.fit_transform(data_samples)
-    print("done in %0.3fs." % (time() - t0))
-    print()
-
-    # Fit the NMF model
-    print(
-        "Fitting the NMF model (Frobenius norm) with tf-idf features, "
-        "n_samples=%d and n_features=%d..." % (n_samples, n_features)
-    )
-    t0 = time()
-    nmf = NMF(n_components=n_components, random_state=1, alpha=0.1, l1_ratio=0.5).fit(tfidf)
-    print("done in %0.3fs." % (time() - t0))
-
-    tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
-    plot_top_words(
-        nmf, tfidf_feature_names, n_top_words, "Topics in NMF model (Frobenius norm)"
-    )
-
-    # Fit the NMF model
-    print(
-        "\n" * 2,
-        "Fitting the NMF model (generalized Kullback-Leibler "
-        "divergence) with tf-idf features, n_samples=%d and n_features=%d..."
-        % (n_samples, n_features),
-    )
-    t0 = time()
-    nmf = NMF(
-        n_components=n_components,
-        random_state=1,
-        beta_loss="kullback-leibler",
-        solver="mu",
-        max_iter=1000,
-        alpha=0.1,
-        l1_ratio=0.5,
-    ).fit(tfidf)
-    print("done in %0.3fs." % (time() - t0))
-
-    tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
-    plot_top_words(
-        nmf,
-        tfidf_feature_names,
-        n_top_words,
-        "Topics in NMF model (generalized Kullback-Leibler divergence)",
-    )
-
-    print(
-        "\n" * 2,
-        "Fitting LDA models with tf features, n_samples=%d and n_features=%d..."
-        % (n_samples, n_features),
-    )
-    lda = LatentDirichletAllocation(
-        n_components=n_components,
-        max_iter=5,
-        learning_method="online",
-        learning_offset=50.0,
-        random_state=0,
-    )
-    t0 = time()
-    lda.fit(tf)
-    print("done in %0.3fs." % (time() - t0))
-
-    tf_feature_names = tf_vectorizer.get_feature_names_out()
-    plot_top_words(lda, tf_feature_names, n_top_words, "Topics in LDA model")
-
-
 if __name__ == '__main__':
     dc = DictionaryCreator('eng', 'fra')
-    #dc.dc_preprocessing(save=True)
-    #dc.dc_train_tfidf_based_model(load=True, save=True)
+    # dc.dc_preprocessing(save=True)
+    # dc.dc_train_tfidf_based_model(load=True, save=True)
     dc.dc_evaluate(load=True)
     # sid_with_word_clustering()
