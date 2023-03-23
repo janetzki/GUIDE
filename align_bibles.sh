75f8c848ab7daebf2fcbcdae16a17bda4418eb54
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2023-03-23 11:31:20 +0100

    #14 Use AWESoME word aligner

--- align_bibles.sh
+++ align_bibles.sh
@@ -6,9 +6,41 @@ eval suffix="$3"
 eval base_path="~/repos/mt/dictionary_creator/"
 eval file_path="$(pwd)/$4/${input1_path}_${input2_path}_${suffix}"
 
-"${base_path}"/fast_align/build/fast_align -i "${file_path}.txt" -d -o -v > "${file_path}_forward.align"
-"${base_path}"/fast_align/build/fast_align -i "${file_path}.txt" -d -o -v -r > "${file_path}_reverse.align"
-"${base_path}"/fast_align/build/atools -i "${file_path}_forward.align" -j "${file_path}_reverse.align" -c grow-diag-final-and > "${file_path}_diag.align"
+# # fast_align
+# "${base_path}"/fast_align/build/fast_align -i "${file_path}.txt" -d -o -v > "${file_path}_forward.align"
+# "${base_path}"/fast_align/build/fast_align -i "${file_path}.txt" -d -o -v -r > "${file_path}_reverse.align"
+# "${base_path}"/fast_align/build/atools -i "${file_path}_forward.align" -j "${file_path}_reverse.align" -c grow-diag-final-and > "${file_path}_diag.align"
+#
+# rm "${file_path}_forward.align"
+# rm "${file_path}_reverse.align"
 
-rm "${file_path}_forward.align"
-rm "${file_path}_reverse.align"
+DATA_FILE="${file_path}.txt"
+MODEL_NAME_OR_PATH=bert-base-multilingual-cased
+OUTPUT_FILE="${file_path}_awesome.align"
+
+# code from https://github.com/neulab/awesome-align/blob/master/README.md
+# fine-tuning
+#CUDA_VISIBLE_DEVICES=0 awesome-train \
+#    --output_dir=$OUTPUT_DIR \
+#    --model_name_or_path=bert-base-multilingual-cased \
+#    --extraction 'softmax' \
+#    --do_train \
+#    --train_tlm \
+#    --train_so \
+#    --train_data_file=$DATA_FILE \
+#    --per_gpu_train_batch_size 2 \
+#    --gradient_accumulation_steps 4 \
+#    --num_train_epochs 1 \
+#    --learning_rate 2e-5 \
+#    --save_steps 4000 \
+#    --max_steps 20000 #\
+#    #--do_eval \
+#    #--eval_data_file=$EVAL_FILE
+
+CUDA_VISIBLE_DEVICES=0 awesome-align \
+    --output_file=$OUTPUT_FILE \
+    --model_name_or_path=$MODEL_NAME_OR_PATH \
+    --data_file=$DATA_FILE \
+    --extraction 'softmax' \
+    --batch_size 32
+
