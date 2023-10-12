7fcd0b9f063279ef437c6985b29517d4975533ca
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2023-10-12 19:35:32 +0200

    Pass full paths to align_bibles.sh

--- align_bibles.sh
+++ align_bibles.sh
@@ -1,10 +1,7 @@
-# usage: align_bibles.sh <input1_path> <input2_path> <suffix> <output_path>
+# usage: align_bibles.sh <file_path>
 
-eval input1_path="$1"
-eval input2_path="$2"
-eval suffix="$3"
 eval base_path="~/repos/mt/dictionary_creator/"
-eval file_path="$(pwd)/$4/${input1_path}_${input2_path}_${suffix}"
+eval file_path="$(pwd)/$1"
 
 # "${base_path}"/fast_align/build/fast_align -i "${file_path}.txt" -d -o -v > "${file_path}_forward.align"
 # "${base_path}"/fast_align/build/fast_align -i "${file_path}.txt" -d -o -v -r > "${file_path}_reverse.align"
